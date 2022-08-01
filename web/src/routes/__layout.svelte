87d1ad814602bf9f7a752711db83b16b7c4d6e3a
dubs3c <dubs3c@users.noreply.github.com>
2022-08-01 22:55:01 +0200

    changes to frontend

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -2,23 +2,25 @@
     import "../static/global.css"
     import "../static/grid.css"
 </script>
-
+<div class="container">
 <main>
-    <div class="row hr">
-        <div class="col">
-            <h1><a href="/" style="text-decoration:none; color:turquoise;">SAMLA</a></h1>
-        </div>
-        <nav>
+    
+        <div class="row hr">
             <div class="col">
-                <ul>
-                    <li><a href="\settings">Settings</a></li>
-                    <li><a href="\logout" class="logout-red">Logout</a></li>
-                </ul> 
+                <h1><a href="/" style="text-decoration:none; color:turquoise;">SAMLA</a></h1>
             </div>
-        </nav>
-    </div>
+            <nav>
+                <div class="col">
+                    <ul>
+                        <li><a href="\settings">Settings</a></li>
+                        <li><a href="\logout" class="logout-red">Logout</a></li>
+                    </ul> 
+                </div>
+            </nav>
+        </div>
     <br />
 
     <slot></slot>
 
 </main>
+</div>
