63debe4668bf77e7c1c5fcf07f581e1f322bdbef
dubs3c <dubs3c@users.noreply.github.com>
2022-08-06 14:03:01 +0200

    prepare settings page

--- web/src/routes/settings.svelte
+++ web/src/routes/settings.svelte
@@ -1,6 +1,35 @@
+<script lang="ts">
+import Card from "./../components/card/Card.svelte";
+</script>
+
 <svelte:head>
 	<title>Settings</title>
 </svelte:head>
 
-<h1>Settings</h1>
-<p>TODO...</p>
+
+<div class="row">
+	<div class="col-4">
+		<Card>
+			<h3>Settings</h3>
+			<ul>
+				<li>link</li>
+			</ul>
+
+			<h3>Admin</h3>
+			<ul>
+				<li>link</li>
+			</ul>
+	
+		</Card>
+	</div>
+	<div class="col">
+		<h3>Account Information</h3>
+
+		<hr />
+
+		<h3>Change Password</h3>
+		<hr />
+		<h3>Delete Account</h3>
+	</div>
+
+</div>
